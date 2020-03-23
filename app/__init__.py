754e3dace291d43c116d8e7fa5ebac26b9a1716d
Jada-Rae Brown <kourtney_jada@hotmail.com>
2020-03-23 11:49:52 -0500

    change db uri

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Som3$ec5etK*y'  
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://jada:pass@localhost/jada'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://namoqsjyijrgzf:c5118a88511a95899a55134b2ab2794ae0e62178a4b7620b71b51526a724fdd1@ec2-3-91-112-166.compute-1.amazonaws.com:5432/dd07a9q5ufeik7'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
 db = SQLAlchemy(app)
 
